edfaa0002194297774bdbfdbe52c9eed9e258b49
andxbes <andxbes@gmail.com>
2024-12-22 18:56:54 +0200

    53

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
+  @Index({ unique: true })
   email: string;
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User width id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User width id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User width id', this.id);
+  }
 }
