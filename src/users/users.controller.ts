11065d511b15e4562a260a53ef8fe923dd05b016
andxbes <andxbes@gmail.com>
2024-12-24 03:16:12 +0200

    89

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -18,9 +19,11 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(
     private usersService: UsersService,
