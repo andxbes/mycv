970c0198d94acec303f71544626da3b7c7a4cc76
andxbes <andxbes@gmail.com>
2024-12-29 17:28:55 +0200

    116

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupApp } from './setup-app';
+// import { cookieSession } from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupApp(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { setupApp } from '../src/setup-app';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,12 +12,11 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app);
     await app.init();
   });
 
   it('handles a signup request', () => {
-    const tEmail = 'test100@test.com';
+    const tEmail = 'test101@test.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email: tEmail, password: '12345' })
